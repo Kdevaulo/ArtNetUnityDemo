a75e96b3905d9e56634236efd88713ecfee404d6
Никита Михайлов <komnidev@gmail.com>
2022-09-20 22:45:17 +0500

    Painters scripts added

---
+++ Assets/Scripts/StripPainters/DiodPainter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace StripPainters
+{
+    [Serializable]
+    public class DiodPainter : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _renderer;
+
+        [SerializeField] private Light _light;
+
+        public void SetColor(Color32 color)
+        {
+            _renderer.color = color;
+            _light.color = color;
+        }
+    }
+}
---
+++ Assets/Scripts/StripPainters/StripPainter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace StripPainters
+{
+    public class StripPainter : MonoBehaviour
+    {
+        public int DiodsCount => _diodColorizers.Length;
+
+        [SerializeField] private DiodPainter[] _diodColorizers;
+
+        public void FillStrip(Color32[] colors)
+        {
+            for (var i = 0; i < _diodColorizers.Length; i++)
+            {
+                var colorizer = _diodColorizers[i];
+                colorizer.SetColor(colors[i]);
+            }
+        }
+    }
+}
