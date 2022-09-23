5d71f20051d3383d4bc075a96fc32d23d46e1ae4
Nikita Mikhailov <komnidev@gmail.com>
2022-09-24 01:26:27 +0500

    Painter namespaces added

--- Assets/Scripts/Painters/DiodPainter.cs
+++ Assets/Scripts/Painters/DiodPainter.cs
@@ -2,7 +2,7 @@
 
 using UnityEngine;
 
-namespace StripPainters
+namespace Painters
 {
     [Serializable]
     public class DiodPainter : MonoBehaviour
--- Assets/Scripts/Painters/StripPainter.cs
+++ Assets/Scripts/Painters/StripPainter.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-namespace StripPainters
+namespace Painters
 {
     public class StripPainter : MonoBehaviour
     {
