3481400ca3e7065f46daa99f308cc87aa335e81c
Nikita Mikhailov <kdevaulo@gmail.com>
2022-12-30 16:52:30 +0500

    Utils description updated

--- Assets/Scripts/StripExtender/VectorUtils.cs
+++ Assets/Scripts/StripExtender/VectorUtils.cs
@@ -7,9 +7,9 @@ namespace UnityArtNetDemo.StripExtender
     public static class VectorUtils
     {
         /// <summary>
-        /// Returns distance and direction == max float value of distances from startPosition,
+        /// Returns distance and axis == max float value of distances from startPosition,
         /// other axes == 0
-        /// for example Z axis: (15f, Vector3(0f, 0f, 1f)) or  Y axis: (7, Vector3(0f, 1f, 0f))
+        /// for example Z axis: (15f, Vector3(0f, 0f, 1f)) or  Y axis: (7f, Vector3(0f, 1f, 0f))
         /// </summary>
         public static (float, Vector3) GetMaxDistanceAndAxis(Vector3 startPosition, Vector3 targetPosition)
         {
